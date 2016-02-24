62331feb26f7a85e0250141c84c1ba78002f1d38
SergeyBondarenko <sergibondarenko@gmail.com>
2016-02-24 09:44:41 +0100

    Make dist.

--- myplugin/enabled/_31000_myplugin.py
+++ myplugin/enabled/_31000_myplugin.py
@@ -5,16 +5,17 @@ PANEL = 'mypanel'
 PANEL_DASHBOARD = 'identity'
 
 # Python panel class of the PANEL to be added.
-ADD_PANEL = 'myplugin.content.mypanel.panel.MyPanel'
+#ADD_PANEL = 'myplugin.content.mypanel.panel.MyPanel'
+ADD_PANEL = 'openstack_dashboard.dashboards.identity.mypanel.panel.MyPanel'
 
 # A list of applications to be prepended to INSTALLED_APPS
 ADD_INSTALLED_APPS = ['myplugin']
 
-# A list of AngularJS modules to be loaded when Angular bootstraps.
-ADD_ANGULAR_MODULES = ['horizon.dashboard.identity.myplugin.mypanel']
+## A list of AngularJS modules to be loaded when Angular bootstraps.
+#ADD_ANGULAR_MODULES = ['horizon.dashboard.identity.myplugin.mypanel']
 
-# Automatically discover static resources in installed apps
-AUTO_DISCOVER_STATIC_FILES = True
+## Automatically discover static resources in installed apps
+#AUTO_DISCOVER_STATIC_FILES = True
 
-# A list of scss files to be included in the compressed set of files
-ADD_SCSS_FILES = ['dashboard/identity/myplugin/myplugin.scss']
+## A list of scss files to be included in the compressed set of files
+#ADD_SCSS_FILES = ['dashboard/identity/myplugin/myplugin.scss']
